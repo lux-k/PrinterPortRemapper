38e30cd8858ddac2989f3789b907a39ab24f3b62
Kevin D. Lux <luxk@mail.med.upenn.edu>
2022-03-15 13:45:56 -0400

    Added ranges and manual naming

--- PrinterPortRemapper/Program.cs
+++ PrinterPortRemapper/Program.cs
@@ -3,12 +3,62 @@ using System.Collections.Generic;
 using Microsoft.Win32;
 using System.ServiceProcess;
 using System.Net;
+using System.Collections;
+using System.Linq;
 
 // needs to run as administrator because of the registry changes
 // and to restart print spooler
 
 namespace PrinterPortRemapper
 {
+    public enum ProgramMode { ToHost, ToIP, ListPorts };
+    public enum SNMPMode { ForceOff, ForceOn, Ignore };
+
+    class NetworkRange
+    {
+        private IPAddress start;
+        private IPAddress end;
+
+        public NetworkRange()
+        {
+            start = IPAddress.Parse("0.0.0.0");
+            end = IPAddress.Parse("255.255.255.255");
+        }
+
+        public NetworkRange(string s)
+        {
+            if (s.Contains("-"))
+            {
+                string[] arr = s.Split('-');
+                start = IPAddress.Parse(arr[0]);
+                end = IPAddress.Parse(arr[1]);
+            }
+            else
+            {
+                start = IPAddress.Parse(s);
+                end = IPAddress.Parse(s);
+            }
+        }
+
+        public bool InRange(IPAddress a)
+        {
+            UInt32 ipStart = BitConverter.ToUInt32(start.GetAddressBytes().Reverse().ToArray(), 0);
+            UInt32 ipEnd = BitConverter.ToUInt32(end.GetAddressBytes().Reverse().ToArray(), 0);
+            UInt32 ip = BitConverter.ToUInt32(a.GetAddressBytes().Reverse().ToArray(), 0);
+            return ipStart <= ip && ip <= ipEnd;
+        }
+
+        public bool InRange(string s)
+        {
+            return InRange(IPAddress.Parse(s));
+        }
+
+        public override string ToString()
+        {
+            return start.ToString() + " - " + end.ToString();
+        }
+    }
+
     class PortStatus
     {
         private bool _c;
@@ -21,7 +71,7 @@ namespace PrinterPortRemapper
         {
             Changed = c;
         }
-        public PortStatus(bool c, string n )
+        public PortStatus(bool c, string n)
         {
             Changed = c;
             NewName = n;
@@ -58,32 +108,10 @@ namespace PrinterPortRemapper
         const string BASE = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Print\\Printers";
         delegate PortStatus PortProcessor(string s);
         static Dictionary<string, PortProcessor> PortMap;
-        static Boolean _toIP = false;
-        static Boolean _toHost = true;
-
-        public static bool ToIP
-        {
-            get
-            {
-                return _toIP;
-            }
-            set
-            {
-                _toIP = value;
-            }
-        }
-
-        public static bool ToHost
-        {
-            get
-            {
-                return _toHost;
-            }
-            set
-            {
-                _toHost = value;
-            }
-        }
+        static ProgramMode Mode = ProgramMode.ListPorts;
+        static SNMPMode SNMP = SNMPMode.Ignore;
+        static ArrayList _networkRanges = new ArrayList();
+        static Dictionary<string, string> ManualDictionary = new Dictionary<string, string>();
 
         static void Main(string[] args)
         {
@@ -91,22 +119,44 @@ namespace PrinterPortRemapper
             Logger.Log("Program log start: " + DateTime.Now);
             Logger.Log("Machine name: " + Environment.MachineName);
             Logger.Log("User name: " + Environment.UserName);
-            if (args.Length >= 1)
+
+
+            foreach (string s in args)
             {
-                if (args[0].Equals("toip"))
-                { ToIP = true; ToHost = false; }
-                if (args[0].Equals("tohost"))
-                { ToHost = true; ToHost = false; }
-                if (args[0].Equals("listports"))
-                { ToHost = false; ToHost = false; }
-            } else
-            {
-                Logger.Log("No options found, assuming listports.");
-                ToHost = false; ToHost = false;
+                if (s.Equals("toip"))
+                    Mode = ProgramMode.ToIP;
+                else if (s.Equals("tohost"))
+                    Mode = ProgramMode.ToHost;
+                else if (s.Equals("disablesnmp"))
+                    SNMP = SNMPMode.ForceOff;
+                else if (s.Equals("enablesnmp"))
+                    SNMP = SNMPMode.ForceOn;
+                else if (s.Equals("ignoresnmp"))
+                    SNMP = SNMPMode.Ignore;
+                else if (s.StartsWith("range:"))
+                {
+                    foreach (string range in s.Substring(6).Split(','))
+                    {
+                        _networkRanges.Add(new NetworkRange(range));
+                    }
+                }
+                else if (s.StartsWith("rename:"))
+                {
+                    foreach (string range in s.Substring(7).Split(','))
+                    {
+                        string[] arr = range.Split('=');
+                        ManualDictionary.Add(arr[0], arr[1]);
+                    }
+                }
             }
 
-            Logger.Log("Translating to IP: " + ToIP);
-            Logger.Log("Translating to Hostname: " + ToHost);
+            if (_networkRanges.Count == 0)
+                _networkRanges.Add(new NetworkRange());
+
+            Logger.Log("Translating to IP: " + (Mode == ProgramMode.ToIP).ToString());
+            Logger.Log("Translating to Hostname: " + (Mode == ProgramMode.ToHost).ToString());
+            Logger.Log("SNMP flag: " + (SNMP.ToString()));
+            Logger.Log("Valid ranges: " + String.Join(", ", _networkRanges.ToArray().Select( el => el.ToString())));
 
             PortMap = LoadPortMap();
             bool changes = false;
@@ -120,6 +170,38 @@ namespace PrinterPortRemapper
                 RestartSpooler();
 
             Logger.Cleanup();
+        }
+
+        static bool InAllowedRanges(IPAddress i)
+        {
+            foreach (NetworkRange range in _networkRanges)
+            {
+                bool res = range.InRange(i);
+                if (res)
+                {
+                    Logger.Log("Process " + i.ToString() + " : " + range.ToString());
+                    return true;
+                }
+
+            }
+
+            Logger.Log(i.ToString() + " not found on allowed network ranges.");
+            return false;
+        }
+
+        static bool InAllowedRanges(string s)
+        {
+            IPAddress i = null;
+            IPAddress.TryParse(s, out i);
+            if (i == null)
+            {
+                return false;
+            }
+            else
+            {
+                return InAllowedRanges(i);
+            }
+
         }
 
         static Dictionary<string, PortProcessor> LoadPortMap()
@@ -143,14 +225,14 @@ namespace PrinterPortRemapper
                             PortMap[p] = new PortProcessor(StandardPort);
                         }
                     }
-                } 
-                
+                }
+
             }
             Logger.Log("---");
             return PortMap;
         }
 
-        static String[] GetPrinters( )
+        static String[] GetPrinters()
         {
             RegistryKey cc = Registry.LocalMachine.OpenSubKey(BASE);
             String[] names = cc.GetSubKeyNames();
@@ -191,13 +273,14 @@ namespace PrinterPortRemapper
             Logger.Log("Processing " + s + " as a WSD port.");
             String guid;
             PortStatus res = new PortStatus();
-            RegistryKey r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum\\SWD\\PRINTENUM\\" + s);
+            RegistryKey reg = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum\\SWD\\PRINTENUM\\" + s, true);
             String host = null;
-            if (r != null && r.GetValue("ContainerID") != null) {
-                guid = r.GetValue("ContainerID").ToString();
+            if (reg != null && reg.GetValue("ContainerID") != null)
+            {
+                guid = reg.GetValue("ContainerID").ToString();
                 guid = guid.Substring(1, guid.Length - 2);
                 Logger.Log("ContainerID is " + guid);
-                r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum\\SWD\\DAFWSDProvider");
+                RegistryKey r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum\\SWD\\DAFWSDProvider", true);
                 if (r != null)
                 {
                     foreach (String n in r.GetSubKeyNames())
@@ -211,34 +294,56 @@ namespace PrinterPortRemapper
                             Uri u = new Uri(loc);
                             System.Net.IPAddress.TryParse(u.Host, out IPAddress ip);
 
-                            if (ToHost && ip != null ) 
-                                    host = IPtoHostname(ip);
-                            else if (ToIP && ip == null)
-                                    host = HostnameToIP(ip.ToString());
+                            if (Mode == ProgramMode.ToHost && ip != null && InAllowedRanges(ip))
+                                host = IPtoHostname(ip);
+                            else if (Mode == ProgramMode.ToIP && ip == null)
+                            {
+                                host = HostnameToIP(u.Host);
+                                if (!InAllowedRanges(host))
+                                    host = null;
+                            }
 
                             if (host != null)
-                            { 
-                                String newloc = loc.Replace(u.Host, host);
+                            {
+                                String newloc = ReplaceIgnoreCase(loc, u.Host, host);
                                 Logger.Log("Changing URL from " + loc + " to " + newloc);
                                 kid.SetValue("LocationInformation", newloc);
                                 res.Changed = true;
-                            } 
+                            }
+                        }
+                    }
+                }
+
+                if (res.Changed)
+                {
+                    if (reg != null && reg.GetValue("LocationInformation") != null)
+                    {
+                        String loc = reg.GetValue("LocationInformation").ToString();
+                        Logger.Log("WSD URI: " + loc);
+                        Uri u = new Uri(loc);
+                        String newloc = ReplaceIgnoreCase(loc, u.Host, host);
+                        if (!newloc.Equals(loc))
+                        {
+                            Logger.Log("Changing URL from " + loc + " to " + newloc);
+                            reg.SetValue("LocationInformation", newloc);
                         }
                     }
                 }
             }
-
-            if (res.Changed)
-            {
-                String loc = r.GetValue("LocationInformation").ToString();
-                Logger.Log("WSD URI: " + loc);
-                Uri u = new Uri(loc);
-                String newloc = loc.Replace(u.Host, host);
-                Logger.Log("Changing URL from " + loc + " to " + newloc);
-                r.SetValue("LocationInformation", newloc);
-            }
-
             return res;
+        }
+
+        static string ReplaceIgnoreCase(string s, string word, string replace)
+        {
+            int index = s.IndexOf(word, StringComparison.OrdinalIgnoreCase);
+
+            // perform the replace on the matched word
+            if (index >= 0)
+            {
+                s = s.Remove(index, word.Length);
+                s = s.Insert(index, replace);
+            }
+            return s;
         }
 
         static PortStatus StandardPort(string s)
@@ -247,11 +352,31 @@ namespace PrinterPortRemapper
 
             RegistryKey r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Print\\Monitors\\Standard TCP/IP Port\\Ports\\" + s, true);
 
+
+            if (SNMP != SNMPMode.Ignore)
+            {
+                string myKey = "SNMP Enabled";
+                if (r.GetValue(myKey) != null)
+                {
+                    string snmpVal = r.GetValue(myKey).ToString();
+                    if (snmpVal.Equals("1") && SNMP == SNMPMode.ForceOff)
+                    {
+                        Logger.Log("Turning OFF SNMP...");
+                        r.SetValue(myKey, 0);
+                    }
+                    else if (snmpVal.Equals("0") && SNMP == SNMPMode.ForceOn)
+                    {
+                        Logger.Log("Turning ON SNMP...");
+                        r.SetValue(myKey, 1);
+                    }
+                }
+            }
+
             if (r != null && r.GetValue("HostName") != null)
             {
                 string val = r.GetValue("HostName").ToString();
                 System.Net.IPAddress.TryParse(val, out IPAddress ip);
-                if (ToHost && ip != null)
+                if (Mode == ProgramMode.ToHost && ip != null && InAllowedRanges(ip))
                 {
                     string host = IPtoHostname(ip);
                     if (host != null)
@@ -262,14 +387,16 @@ namespace PrinterPortRemapper
                         if (!val.Equals(host))
                         {
                             r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Print\\Monitors\\Standard TCP/IP Port\\Ports", true);
-                            RenameSubKey(r, s, "PMACS_" + nn);
+                            nn = "PMACS_" + nn;
+                            RenameSubKey(r, s, nn);
                         }
                         return new PortStatus(true, nn);
                     }
-                } else if (ToIP && ip == null)
+                }
+                else if (Mode == ProgramMode.ToIP && ip == null)
                 {
-                    string host = HostnameToIP(ip.ToString());
-                    if (host != null)
+                    string host = HostnameToIP(val);
+                    if (host != null && InAllowedRanges(host))
                     {
                         r.SetValue("HostName", host);
                         Logger.Log("Changing hostname from " + val + " to " + host);
@@ -277,7 +404,8 @@ namespace PrinterPortRemapper
                         if (!val.Equals(host))
                         {
                             r = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Print\\Monitors\\Standard TCP/IP Port\\Ports", true);
-                            RenameSubKey(r, s, "PMACS_" + nn);
+                            nn = "PMACS_" + nn;
+                            RenameSubKey(r, s, nn);SY
                         }
                         return new PortStatus(true, nn);
                     }
@@ -291,11 +419,13 @@ namespace PrinterPortRemapper
             if (s.StartsWith("WSD-"))
             {
                 return WSDPort(s);
-            } else {
+            }
+            else
+            {
                 // invoked the mapped processor
                 if (PortMap.ContainsKey(s))
                 {
-                   return PortMap[s](s);
+                    return PortMap[s](s);
                 }
             }
 
@@ -304,6 +434,12 @@ namespace PrinterPortRemapper
 
         static string HostnameToIP(string host)
         {
+            if (ManualDictionary.ContainsKey(host))
+            {
+                Logger.Log("Using manual entry of " + host + " --> " + ManualDictionary[host]);
+                return ManualDictionary[host];
+            }
+
             string ip = null;
             try
             {
@@ -322,6 +458,12 @@ namespace PrinterPortRemapper
 
         static string IPtoHostname(IPAddress ip)
         {
+            if (ManualDictionary.ContainsKey(ip.ToString()))
+            {
+                Logger.Log("Using manual entry of " + ip.ToString() + " --> " + ManualDictionary[ip.ToString()]);
+                return ManualDictionary[ip.ToString()];
+            }
+
             IPHostEntry hostInfo = null;
             try
             {
@@ -332,14 +474,15 @@ namespace PrinterPortRemapper
             if (hostInfo != null && hostInfo.HostName != null)
             {
                 return hostInfo.HostName;
-            } else
+            }
+            else
             {
                 Logger.Log("Unable to find a hostname for " + ip);
             }
             return null;
         }
 
-        static string GetHostname(string s )
+        static string GetHostname(string s)
         {
             return s.Substring(0, s.IndexOf('.'));
         }
--- PrinterPortRemapper/readme.txt
+++ PrinterPortRemapper/readme.txt
@@ -1,4 +1,5 @@
 Log file: c:\printer-remapper.txt
+You have to run as admin on the system to restart services/edit registry.
 
 Command line options:
 
@@ -7,4 +8,38 @@ Command line options:
 	toip		translate printer ports to IP addresses
 	tohost		translate printer ports to DNS hostnames
 	listports	only list the printer ports found *
-			* default option
+
+	ignoresnmp	Don't worry about SNMP settings on the port *
+	enablesnmp	Turn on SNMP settings for port
+	disablesnmp	Turn off SNMP settings for port
+			* default options
+
+	Manually specify a newname for an old name.
+	rename:oldname=newname
+	
+	Only change printers with IPs or hostnames that fall into this range
+	range:1.2.3.4
+	range:1.2.3.4-5.5.5.5	
+	range:1.2.3.4,1.2.3.5
+
+	e.g.
+	PrinterPortRemapper tohost
+		For any printer port
+			That needs to be converted to hostname
+				Do so using DNS
+
+
+	PrinterPortRemapper tohost range:1.1.0.0-2.0.0.0 rename:printer1=1.2.3.4
+		For any printer port
+			On the network range of 1.1.0.0 through 2.0.0.0
+				That needs to be converted to hostname
+					Do so using dns, EXCEPT printer1 (use given name)
+
+	To only rename one printer manually:
+	PrinterPortRemapper tohost range:6.6.6.6 rename:6.6.6.6:myawesomeprinter
+
+	To rename all printers to hostnames, given one explicit name:
+	PrinterPortRemapper tohost rename:6.6.6.6:myawesomeprinter
+
+	More than likely, if you want to impact only one printer, you want a range that matches. Otherwise you are likely
+	doing more work than you thought.
